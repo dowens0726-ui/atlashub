efe928b947869aa1c099230f8f52682c47ba8670
Daniel Owens <daniel@example.com>
2026-07-02 21:55:27 -0600

    Remove legacy map filters

--- app/components/map/index.ts
+++ app/components/map/index.ts
@@ -1,4 +1,3 @@
 export { default as MapCanvas } from "./MapCanvas";
 export { default as MapMarker } from "./MapMarker";
-export { default as MapFilters } from "./MapFilters";
 export { default as MapSidebar } from "./MapSidebar";
