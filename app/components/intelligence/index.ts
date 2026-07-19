8d89f9d058679ac18ead1a42da4b22a76965b6ca
Daniel Owens <daniel@example.com>
2026-07-19 01:23:19 -0600

    refactor: organize intelligence component exports

--- app/components/intelligence/index.ts
+++ app/components/intelligence/index.ts
@@ -1,155 +1,9 @@
-export {
-  default as AtlasAIPanel,
-} from "./AtlasAIPanel";
-
-export {
-  default as AtlasCoachCard,
-} from "./AtlasCoachCard";
-
-export {
-  default as AtlasLiveEventToast,
-} from "./AtlasLiveEventToast";
-
-export {
-  default as AtlasIntelligenceTimelineCard,
-} from "./AtlasIntelligenceTimelineCard";
-
-export {
-  default as AtlasRecommendationAnalysisCard,
-} from "./AtlasRecommendationAnalysisCard";
-
-export {
-  default as AtlasCoreCard,
-} from "./AtlasCoreCard";
-
-export {
-  default as AtlasDecisionControls,
-} from "./AtlasDecisionControls";
-
-export {
-  default as AtlasOutcomeReportCard,
-} from "./AtlasOutcomeReportCard";
-
-export {
-  default as AtlasBehaviorCard,
-} from "./AtlasBehaviorCard";
-
-export {
-  default as AtlasStrategyReportCard,
-} from "./AtlasStrategyReportCard";
-
-export {
-  default as AtlasAdvisorCard,
-} from "./AtlasAdvisorCard";
-
-export {
-  default as AtlasDailyObjectivesCard,
-} from "./AtlasDailyObjectivesCard";
-
-export {
-  default as AtlasIntelligenceFeed,
-} from "./AtlasIntelligenceFeed";
-
-export {
-  default as AtlasMemoryCard,
-} from "./AtlasMemoryCard";
-
+/*
+ * Shared intelligence primitives.
+ */
 export {
   default as AtlasSection,
 } from "./AtlasSection";
-
-export {
-  default as AtlasMemoryInsightCard,
-} from "./AtlasMemoryInsightCard";
-
-export {
-  default as AtlasEvolutionCard,
-} from "./AtlasEvolutionCard";
-
-export {
-  default as AtlasMissionEvolutionCard,
-} from "./AtlasMissionEvolutionCard";
-
-export {
-  default as AtlasIdentityCard,
-} from "./AtlasIdentityCard";
-
-export {
-  default as AtlasOutcomeCard,
-} from "./AtlasOutcomeCard";
-
-export {
-  default as AtlasActionTrackerCard,
-} from "./AtlasActionTrackerCard";
-
-export {
-  default as AtlasValidationCard,
-} from "./AtlasValidationCard";
-
-export {
-  default as AtlasStatusCard,
-} from "./AtlasStatusCard";
-
-export {
-  default as AtlasStrategyInsightCard,
-} from "./AtlasStrategyInsightCard";
-
-export {
-  default as AtlasStrategyFeedbackCard,
-} from "./AtlasStrategyFeedbackCard";
-
-export {
-  default as AtlasStrategicPlanCard,
-} from "./AtlasStrategicPlanCard";
-
-export {
-  default as EmpireForecastCard,
-} from "./EmpireForecastCard";
-
-export {
-  default as EmpireSimulatorCard,
-} from "./EmpireSimulatorCard";
-
-export {
-  default as EmpireTimelineCard,
-} from "./EmpireTimelineCard";
-
-export {
-  default as AtlasSessionPlanCard,
-} from "./AtlasSessionPlanCard";
-
-export {
-  default as PersonalPicksCard,
-} from "./PersonalPicksCard";
-
-
-/*
- * Existing intelligence exports.
- * Keep these because vehicle and weapon detail pages depend on them.
- */
-export {
-  default as AtlasIntelligenceCard,
-} from "./AtlasIntelligenceCard";
-
-export {
-  default as VehicleRecommendations,
-} from "./VehicleRecommendations";
-
-export {
-  default as VehicleIntelligenceCard,
-} from "./VehicleIntelligenceCard";
-
-export { 
-  default as AtlasRoadmapCard 
-} from "./AtlasRoadmapCard";
-
-export { 
-  default as AtlasRelationshipPanel 
-} from "./AtlasRelationshipPanel";
-
-export { 
-  default as AtlasReasoningPanel 
-} from "./AtlasReasoningPanel";
 
 export {
   default as AtlasStatusBadge,
@@ -166,3 +20,174 @@ export {
 export type {
   AtlasIntelligencePanelTone,
 } from "./AtlasIntelligencePanel";
+
+export {
+  default as AtlasMetricCard,
+} from "./AtlasMetricCard";
+
+export type {
+  AtlasMetricCardTone,
+} from "./AtlasMetricCard";
+
+/*
+ * Core intelligence surfaces.
+ */
+export {
+  default as AtlasAIPanel,
+} from "./AtlasAIPanel";
+
+export {
+  default as AtlasCoachCard,
+} from "./AtlasCoachCard";
+
+export {
+  default as AtlasCoreCard,
+} from "./AtlasCoreCard";
+
+export {
+  default as AtlasAdvisorCard,
+} from "./AtlasAdvisorCard";
+
+export {
+  default as AtlasStatusCard,
+} from "./AtlasStatusCard";
+
+export {
+  default as AtlasBehaviorCard,
+} from "./AtlasBehaviorCard";
+
+export {
+  default as AtlasMemoryCard,
+} from "./AtlasMemoryCard";
+
+export {
+  default as AtlasMemoryInsightCard,
+} from "./AtlasMemoryInsightCard";
+
+export {
+  default as AtlasIdentityCard,
+} from "./AtlasIdentityCard";
+
+export {
+  default as AtlasEvolutionCard,
+} from "./AtlasEvolutionCard";
+
+export {
+  default as AtlasMissionEvolutionCard,
+} from "./AtlasMissionEvolutionCard";
+
+/*
+ * Strategy, decision, and outcome intelligence.
+ */
+export {
+  default as AtlasRecommendationAnalysisCard,
+} from "./AtlasRecommendationAnalysisCard";
+
+export {
+  default as AtlasDecisionControls,
+} from "./AtlasDecisionControls";
+
+export {
+  default as AtlasOutcomeReportCard,
+} from "./AtlasOutcomeReportCard";
+
+export {
+  default as AtlasOutcomeCard,
+} from "./AtlasOutcomeCard";
+
+export {
+  default as AtlasActionTrackerCard,
+} from "./AtlasActionTrackerCard";
+
+export {
+  default as AtlasValidationCard,
+} from "./AtlasValidationCard";
+
+export {
+  default as AtlasStrategyReportCard,
+} from "./AtlasStrategyReportCard";
+
+export {
+  default as AtlasStrategyInsightCard,
+} from "./AtlasStrategyInsightCard";
+
+export {
+  default as AtlasStrategyFeedbackCard,
+} from "./AtlasStrategyFeedbackCard";
+
+export {
+  default as AtlasStrategicPlanCard,
+} from "./AtlasStrategicPlanCard";
+
+export {
+  default as AtlasSessionPlanCard,
+} from "./AtlasSessionPlanCard";
+
+export {
+  default as AtlasRoadmapCard,
+} from "./AtlasRoadmapCard";
+
+export {
+  default as AtlasRelationshipPanel,
+} from "./AtlasRelationshipPanel";
+
+export {
+  default as AtlasReasoningPanel,
+} from "./AtlasReasoningPanel";
+
+/*
+ * Timeline, feed, and live intelligence.
+ */
+export {
+  default as AtlasLiveEventToast,
+} from "./AtlasLiveEventToast";
+
+export {
+  default as AtlasIntelligenceTimelineCard,
+} from "./AtlasIntelligenceTimelineCard";
+
+export {
+  default as AtlasIntelligenceFeed,
+} from "./AtlasIntelligenceFeed";
+
+export {
+  default as AtlasDailyObjectivesCard,
+} from "./AtlasDailyObjectivesCard";
+
+/*
+ * Empire intelligence.
+ */
+export {
+  default as EmpireForecastCard,
+} from "./EmpireForecastCard";
+
+export {
+  default as EmpireSimulatorCard,
+} from "./EmpireSimulatorCard";
+
+export {
+  default as EmpireTimelineCard,
+} from "./EmpireTimelineCard";
+
+/*
+ * Personalized recommendations.
+ */
+export {
+  default as PersonalPicksCard,
+} from "./PersonalPicksCard";
+
+/*
+ * Existing intelligence exports.
+ * Keep these because vehicle and weapon detail pages depend on them.
+ */
+export {
+  default as AtlasIntelligenceCard,
+} from "./AtlasIntelligenceCard";
+
+export {
+  default as VehicleRecommendations,
+} from "./VehicleRecommendations";
+
+export {
+  default as VehicleIntelligenceCard,
+} from "./VehicleIntelligenceCard";
